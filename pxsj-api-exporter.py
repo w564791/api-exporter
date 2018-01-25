f64c41ded9a791cec51d4e37a154955c73ada22e
kinglong <how_bjl@live.cn>
2018-01-25 17:03:17 +0800

    优化

--- pxsj-api-exporter.py
+++ pxsj-api-exporter.py
@@ -7,7 +7,7 @@ time: 17-12-25 上午10:27
 """
 from prometheus_client import start_http_server,Gauge,Summary
 import sys
-import time
+import time,datetime
 import yaml,json,requests
 import re
 ConnectTimeout = requests.exceptions.ConnectTimeout
@@ -35,24 +35,24 @@ def get_args(args_list):
             try:
                 args = int(Arg[1])
             except ValueError as e:
-                print(e.args)
+                print(datetime.datetime.now(),e.args)
                 exit(1)
             except IndexError as e:
-                print(e.args)
+                print(datetime.datetime.now(),e.args)
                 exit(2)
         elif "-e" in Arg:
             try:
                 cluster_env = Arg[1]
             except IndexError as e:
                 cluster_env = 'swarm'
-                print(e.args)
+                print(datetime.datetime.now(),e.args)
                 exit(3)
         elif "-f" in Arg:
             try:
                 config_file = Arg[1]
             except IndexError as e:
                 config_file = 'config.yml'
-                print(e.args)
+                print(datetime.datetime.now(),e.args)
                 exit(4)
         elif "help" or "-h" in Arg:
             print(
@@ -69,13 +69,13 @@ def get_args(args_list):
 
 
 args,cluster_env,config_file = get_args(args_list)
-print_green("args list ----> %s"%args_list)
-print_green("config file ----> %s"%config_file)
-print_green("cluster env ----> %s"%cluster_env)
+print_green("%s: args list ----> %s"%(datetime.datetime.now(),args_list))
+print_green("%s: config file ----> %s"%(datetime.datetime.now(),config_file))
+print_green("%s cluster env ----> %s"%(datetime.datetime.now(),cluster_env))
 
 def http_ok(url,result,data,resbonse,projectCode):
     """当获取resbonse成功时，为result赋值"""
-    print("%s get [\033[0;32;10mOK\033[0m]! %s" % (url,resbonse.status_code))
+    print("%s: %s get [\033[0;32;10mOK\033[0m]! %s" % (datetime.datetime.now(),url,resbonse.status_code))
     for i in data['metrics']:
         if i == "http_code":
             result[i] = resbonse.status_code
@@ -84,7 +84,7 @@ def http_ok(url,result,data,resbonse,projectCode):
     return result
 def http_timeout(url,result,data):
     """当获取resbonse超时时，为result赋值为0"""
-    print("%s get [\033[0;31;10mError\033[0m]! " % url)
+    print("%s: %s get [\033[0;31;10mError\033[0m]! " % (datetime.datetime.now(),url))
     for i in data['metrics']:
         if i == "http_code":
             result[i] = -1
@@ -101,64 +101,118 @@ def Scan_conf(config_file):
         try:
             data.append(next(ff))
         except ScannerError    as scanner_error:
-            print_red(scanner_error.problem_mark)
+            print_red("%s: %s"%(datetime.datetime.now(),scanner_error.problem_mark))
             break
         except StopIteration as stop_err:
             # print(stop_err.value)
             break
-    '''格式化yaml中的metrics为字典（原为list）'''
-    # for metrics in data:
-    #     try:
-    #         print(metrics['project'],metrics['metrics'])
-    #         metrics_data = {}
-    #         for  metrics_format in metrics['metrics']:
-    #             metrics_data[metrics_format] = metrics_format
-    #         metrics['metrics'] = metrics_data
-    #     except TypeError:
-    #         pass
 
     return data
 data_list = Scan_conf(config_file)
+def re_data(method,uri,result, datas,*args,**kwargs):
+    try:
+        resbonse = requests.request(method,**kwargs)
 
+        http_ok(uri,result, datas,resbonse,*args)
+    except:
+        http_timeout(uri,result, datas)
+        resbonse = None
+
+    return resbonse
+
+def get_key(data,key,type=False):
+    if type:
+        try:
+            get_data = type(data[key])
+        except:
+            get_data = False
+    else:
+        try:
+            get_data = data[key]
+        except:
+            get_data = False
+    return get_data
 def Result(data_list,cluster_env):
     """获取url的返回值"""
     get_code = []
     for data in data_list:
-        # print(data)
+        auth = get_key(data,'authData',type=tuple)
+        internet = get_key(data,'internet')
+
         if data:
-            if cluster_env == "k8s":
-                url = "%s://%s.%s:%s%s" % (data["protocol"], data["host"], data['namespace'],data["port"], data["url"])
-            elif cluster_env == "swarm":
-                url = "%s://%s:%s%s" % (data["protocol"], data["host"], data["port"], data["url"])
-            elif cluster_env == "local":
-                url = "%s://192.168.31.233:%s%s" % (data["protocol"],data["port"], data["url"])
-            metadata = data['data']
-            method = data['method']
-            header = data['header']
-            projectCode = data['projectCode']
-            result = {}
-            result['program'] = data['program']
-            result['env'] = cluster_env
-            result['project'] = data['project']
-            result['metrics'] = data['metrics']
-            """尝试获取resbonse"""
+            try:
+                if cluster_env == "k8s":
+                    if internet:
+                        url = "%s://%s:%s%s" % (data["protocol"],
+                                                data["host"],
+                                                data["port"],
+                                                data["url"])
+                    else:
+                        url = "%s://%s.%s:%s%s" % (data["protocol"],
+                                                   data["host"],
+                                                   data['namespace'],
+                                                   data["port"],
+                                                   data["url"])
+                elif cluster_env == "swarm":
+                    url = "%s://%s:%s%s" % (data["protocol"],
+                                            data["host"],
+                                            data["port"],
+                                            data["url"])
+                elif cluster_env == "local":
+                    url = "%s://192.168.31.233:%s%s" % (data["protocol"],
+                                                        data["port"],
+                                                        data["url"])
+
+                metadata = data['data']
+                method = data['method'].lower()
+                header = data['header']
+                projectCode = data['projectCode']
+                result = {}
+                result['program'] = data['program']
+                result['env'] = cluster_env
+                result['project'] = data['project']
+                result['metrics'] = data['metrics']
+                # auth = data['auth']
+
+            except KeyError as e:
+                print_red("Key Error, Lost config: [ \"%s\" ]"%e.args)
+                exit(5)
+
+            '''
+            尝试获取resbonse
+            
             if method == 'POST':
+                
                 try:
                     resbonse = requests.post(url=url, data=json.dumps(metadata), headers=header,timeout=2)
                     http_ok(url,result,data, resbonse, projectCode)
                 except  :
 
                     http_timeout(url,result, data)
+                '''
+            re_data(method,url,result,data,
+                    projectCode,url=url,
+                    data=json.dumps(metadata),
+                    headers=header,
+                    timeout=2,
+                    auth=auth
+                    )
 
                     # continue
-            elif method == "GET":
+            '''
+            # elif method == "GET":
+                
+                if auth:
+                    pass
                 try:
                     resbonse = requests.get(url=url,headers=header,timeout=2)
                     http_ok(url,result,data, resbonse, projectCode)
                 except :
                     http_timeout(url,result, data)
-
+                
+                # re_data(requests.get,url,result, data, projectCode,url=url,data=json.dumps(metadata),headers=header,timeout=3,auth=auth)
                     # continue
+                    '''
             get_code.append(result)
     return get_code
 
@@ -195,12 +249,12 @@ def get_REQUEST(RESOULT,args,cluster_env,STATUS):
             # RESOULT
     # print(result,244)
     """重新为Gauge赋值"""
-    print(RESOULT)
+    print(datetime.datetime.now(),RESOULT)
     for key,value in RESOULT.items():
         # print(key,value,"========>")
         # value["Gauge"].labels(env=value["ENV"]).set(value["resbonse_data"])
         # STATUS.labels(value["ENV"],key).set(value["resbonse_data"])
-        print_yellow(value)
+        print_yellow("%s: %-40s %s"%(datetime.datetime.now(),key,value))
         if value["resbonse_data"] == value["default"]:
             STATUS.labels(value["ENV"],key).set(1)
         else:
@@ -213,7 +267,7 @@ if __name__ == '__main__':
 #     # Start up the server to expose the metrics.
     start_http_server(8000)
 #     # Generate some requests.
-    print("start server at port [\033[0;32;10m8000\033[0m]")
+    print("%s: start server at port [\033[0;32;10m8000\033[0m]"%datetime.datetime.now())
     while True:
 
         get_REQUEST(RESOULT,args,cluster_env,UP_STATUS)
